1ec419efb65efe73bfb114710bfa505b55373492
Manish Kumar <manish.login01@gmail.com>
2024-09-15 00:01:12 +0530

    added delete profile feature

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -100,3 +100,45 @@ export const updatePassword = async (req, res) => {
     res.status(500).send(err.message);
   }
 };
+
+// to display delete page
+export const getDeletePage = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    res.render("deleteProfile", { user });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err.message);
+  }
+};
+
+export const deleteProfile = async (req, res) => {
+  try {
+    const { username } = req.params;
+    const { password } = req.body;
+
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).send("Password is incorrect");
+    } else {
+      await User.findOneAndDelete({ username });
+      res.clearCookie("token");
+      res.redirect("/");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err.message);
+  }
+};
--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
-import { displayProfile, displayEditProfile, updateProfile, changePassword, updatePassword } from "../controllers/profileController.js";
+import { displayProfile, displayEditProfile, updateProfile, changePassword, updatePassword, getDeletePage, deleteProfile } from "../controllers/profileController.js";
 
 const profileRouter = express.Router();
 
@@ -20,5 +20,11 @@ profileRouter.get("/:username/change-password", changePassword);
 
 // to update password
 profileRouter.post("/:username/update-password", updatePassword);
+
+// to display delete profile page
+profileRouter.get("/:username/delete-profile", getDeletePage);
+
+// to delete the profile
+profileRouter.post("/:username/delete-profile", deleteProfile);
 
 export default profileRouter;
