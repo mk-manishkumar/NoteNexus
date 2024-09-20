9f9f615b6a139e874542714ef7d35f5134b188c2
Manish Kumar <manish.login01@gmail.com>
2024-09-20 21:53:02 +0530

    added error handler in server.js and created 2 error pages for showcasing error information

--- server.js
+++ server.js
@@ -36,4 +36,15 @@ app.use("/", notesRouter);
 app.use("/", binRouter);
 app.use("/", archiveRouter);
 
+// Handle 404 errors (not found)
+app.use((req, res, next) => {
+  res.status(404).render("404", { message: "Page not found" });
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).render("error", { message: "Something went wrong!" });
+});
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
