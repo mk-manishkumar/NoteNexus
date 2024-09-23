5b62fc9756f86c46d8651a978a4dab3e5068d360
Manish Kumar <manish.login01@gmail.com>
2024-09-23 16:36:58 +0530

    removed tailwind CDN, added using PostCSS plugin

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./views/**/*.{ejs,html}", "./public/stylesheets/**/*.{css,js}"];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
