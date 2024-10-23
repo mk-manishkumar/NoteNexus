421f3d36ca7a33a358dd86bdb383f31e7b28e302
Manish Kumar <manish.login01@gmail.com>
2024-10-23 21:56:21 +0530

    Implement complete guest user signup process with session expiration and auto-logout

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,3 +4,15 @@ const mobileMenu = document.querySelector(".mobile-menu");
 menuToggle.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
+
+// This script will automatically log the guest out after 10 minutes
+window.onload = function () {
+  const guestMode = document.body.getAttribute("data-guest-mode") === "true";
+
+  if (guestMode) {
+    setTimeout(function () {
+      alert("Your session as a guest has expired. You will now be logged out.");
+      window.location.href = "/";
+    }, 10 * 60 * 1000);
+  }
+};
